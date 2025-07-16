c7c994930b0c0dd8ab379fc2966b76d336b1e9d4
Evannn1218 <bx2181@nyu.edu>
2025-07-17 04:34:11 +0800

    蓝方开机举手

--- src/autons.cpp
+++ src/autons.cpp
@@ -550,6 +550,7 @@ void blueGoldAuton()
   chassis.drive_max_voltage = 12;
   chassis.heading_max_voltage = 12;
   chassis.drive_timeout = 2000;
+  wsSpinToPosition(68, 250, 0, 3);
   doinker_right.set(true); // put doinker down
   chassis.drive_distance(39);
   doinker_right.set(false); // grab mogo with doinker
